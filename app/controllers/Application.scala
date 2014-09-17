1d7d9180c23249a0675042afdfbe512d889c50e8
Miguel Ángel Jareño <maje2@alu.ua.es>
2014-09-18 01:13:33 +0200

    Creación value task y definicion método all()

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,23 @@ import play.api.mvc._
 import models.Task
 import play.api.data._
 import play.api.data.Forms._
+import anorm._
+import anorm.SqlParser._
+import play.api.db._
+import play.api.Play.current
 
 object Application extends Controller {
+
+  val task = {
+    get[Long]("id") ~ 
+    get[String]("label") map {
+      case id~label => Task(id, label)
+    }
+  }
+
+  def all(): List[Task] = DB.withConnection { implicit c =>
+    SQL("select * from task").as(task *)
+  }
 
   val taskForm = Form(
   "label" -> nonEmptyText
