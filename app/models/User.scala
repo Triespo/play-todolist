5a9cb59f015b6cce16c87a6b4573e4e2285b6999
Miguel Ángel Jareño <maje2@alu.ua.es>
2014-10-10 11:49:21 +0200

    Create en user models

--- app/models/User.scala
+++ app/models/User.scala
@@ -16,8 +16,14 @@ object User{
    }
 
    def findUser(user: String): Option[Int] = DB.withConnection { implicit c =>
-      SQL("select * from task_user where login = {user}").on('login -> user).as(Int)
+      SQL("select id from task_user where login = {user}").on('login -> user).as(Int)
+   }
 
+   def create(user: String, label: String){
+      DB.withConnection { implicit c =>
+      SQL("update task set (label) values ({label}) where user = {user}").on(
+        'label -> label, 'user -> user
+      ).executeUpdate()
    }
 
    //ef all(user: String): List[Task]
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,7 +2,10 @@
 # ADD task_user
  
 # --- !Ups
+CREATE SEQUENCE task_user_id_seq;
+
 CREATE TABLE task_user (
+   id Integer NOT NULL DEFAULT nextval(task_user_id_seq)
    login varchar(25) NOT NULL DEFAULT 'anonimo',
    #UNIQUE (login)
 );
@@ -11,5 +14,5 @@ alter table task add constraint fk_task_taskUser foreign key
 (user) references task_user (login) on delete restrict on update restrict;
 
 # --- !Downs
-
+DROP SEQUENCE task_user_id_seq;
 DROP TABLE task_user;
