60c056ac4dc31de36078e61b23f5f399324cb10f
Miguel Ángel Jareño <maje2@alu.ua.es>
2014-10-15 15:06:46 +0200

    Resulta DB para Heroku

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -7,8 +7,13 @@ CREATE TABLE task (
     id integer NOT NULL DEFAULT nextval('task_id_seq'),
     label varchar(255)
 );
+CREATE TABLE task_user (
+   login varchar(25) NOT NULL,
+   PRIMARY KEY(login)
+);
 
-ALTER TABLE task ADD user_name varchar(25) DEFAULT 'anonimo'
+ALTER TABLE task ADD user_name varchar(25) DEFAULT 'anonimo';
+ALTER TABLE task ADD CONSTRAINT fk_task_taskUser FOREIGN KEY (user_name) REFERENCES task_user (login);
 
 # --- !Downs
 
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -3,12 +3,6 @@
  
 # --- !Ups
 
-CREATE TABLE task_user (
-   login varchar(25) NOT NULL
-);
-
-ALTER TABLE task ADD CONSTRAINT fk_task_taskUser FOREIGN KEY (user_name) REFERENCES task_user (login);
-
 INSERT INTO task_user VALUES ('anonimo');
 INSERT INTO task_user VALUES ('miguel');
 INSERT INTO task_user VALUES ('pepe');
