8b0c90eaae40b837a59827f60503d1aa112b4e08
Miguel Ángel Jareño <maje2@alu.ua.es>
2014-11-08 18:18:53 +0100

    Test consulta y creacion tareas

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -8,7 +8,28 @@ import play.api.test.Helpers._
 class ApplicationSpec extends Specification {
 
   "Application" should {
-    
+    "error creacion tarea" in{
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val tarea1 = controllers.Application.addTask("anonimo")(FakeRequest())
+
+        status(tarea1) must equalTo(BAD_REQUEST)
+
+      }
+    }
+    "creacion de una tarea" in{
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+
+        val tarea1 = controllers.Application.addTask("anonimo")(
+          FakeRequest().withFormUrlEncodedBody("id" -> "1", "label" -> "Jabon"))
+        val result = controllers.Application.tasks(FakeRequest())
+
+        status(tarea1) must equalTo(CREATED)
+        status(result) must equalTo(OK)
+        //redirectLocation(result) must equalTo(Some("/tasks"))
+
+        contentAsString(result) must contain("Jabon")
+      }
+    }
     "consulta tarea no disponible" in{
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
         
@@ -17,13 +38,24 @@ class ApplicationSpec extends Specification {
         status(result) must equalTo(NOT_FOUND)
       }
     }
-    /*"consulta de tarea disponible" in{
+    "consulta de una tarea disponible" in{
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
 
-        controllers.Application.addTask("Jabon")
+        val tarea1 = controllers.Application.addTask("anonimo")(
+          FakeRequest().withFormUrlEncodedBody("id" -> "1", "label" -> "Goma"))
+        val result = controllers.Application.consultTask(1)(FakeRequest())
 
-        contentAsString(result) must contain("Jabon")
+        status(result) must equalTo(OK)
+        contentAsString(result) must contain("""{"id":1,"label":"Goma","user_name":"anonimo","task_date":"NoData"}""")
       }
-    }*/
+    }
+
   }   
 }
+
+
+
+
+
+
+
