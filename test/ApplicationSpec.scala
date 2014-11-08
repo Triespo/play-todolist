a5e3a9dd16cc0662ca0744baa0d7fc9865c80213
Miguel Ángel Jareño <maje2@alu.ua.es>
2014-11-08 16:40:50 +0100

    Consulta tarea no disponible

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -8,6 +8,22 @@ import play.api.test.Helpers._
 class ApplicationSpec extends Specification {
 
   "Application" should {
+    
+    "consulta tarea no disponible" in{
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        
+        val result = controllers.Application.consultTask(1)(FakeRequest())
 
+        status(result) must equalTo(NOT_FOUND)
+      }
+    }
+    /*"consulta de tarea disponible" in{
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+
+        controllers.Application.addTask("Jabon")
+
+        contentAsString(result) must contain("Jabon")
+      }
+    }*/
   }   
 }
